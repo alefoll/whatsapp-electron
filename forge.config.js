9423aaebcdb0113b3939ef0296bff671b84883d7
Alexandre Le Foll <alexandre@le-foll.fr>
2021-06-01 21:06:46 +1000

    Version 1.4.1

--- forge.config.js
+++ forge.config.js
@@ -1,17 +1,23 @@
+const path = require("path");
+
 module.exports = {
     makers: [{
-        name: '@electron-forge/maker-squirrel',
+        name: "@electron-forge/maker-squirrel",
+        config: {
+            title: "WhatsApp Electron",
+            iconUrl: path.resolve(__dirname, "./icon.ico"),
+        },
     }],
     packagerConfig: {
         asar: true,
     },
     publishers: [{
-        name: '@electron-forge/publisher-github',
+        name: "@electron-forge/publisher-github",
         config: {
             repository: {
-                owner: 'alefoll',
-                name: 'whatsapp-electron',
+                owner: "alefoll",
+                name: "whatsapp-electron",
             },
-        }
+        },
     }],
 }
