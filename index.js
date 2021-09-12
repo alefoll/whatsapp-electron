952f8a9bb92ad2cb24b9ca8d63bcc8848bec988d
Alexandre Le Foll <alexandre@le-foll.fr>
2021-09-12 16:01:30 +1000

    Version 1.4.3

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.on("second-instance", () => {
     }
 });
 
-const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.69 Safari/537.36";
+const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36";
 
 const store = new Store();
 
